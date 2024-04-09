e76acf6d8ac1bf1313a380e81db20e3825de74f0
ProgrammingIsPunishment <134812626+ProgrammingIsPunishment@users.noreply.github.com>
2024-04-09 18:35:44 -0500

    Began testing new design

---
+++ Triumph/Assets/Test/MapShower.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapShower : MonoBehaviour
+{
+    int width;
+    int height;
+
+    Color32[] remapArr;
+    Texture2D paletteTex;
+
+    Color32 prevColor;
+    bool selectAny = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var material = GetComponent<Renderer>().material;
+        var mainTex = material.GetTexture("_MainTex") as Texture2D;
+        var mainArr = mainTex.GetPixels32();
+
+        width = mainTex.width;
+        height = mainTex.height;
+
+        var main2remap = new Dictionary<Color32, Color32>();
+        remapArr = new Color32[mainArr.Length];
+        int idx = 0;
+        for (int i = 0; i < mainArr.Length; i++)
+        {
+            var mainColor = mainArr[i];
+            if (!main2remap.ContainsKey(mainColor))
+            {
+                var low = (byte)(idx % 256);
+                var high = (byte)(idx / 256);
+                main2remap[mainColor] = new Color32(low, high, 0, 255);
+                idx++;
+            }
+            var remapColor = main2remap[mainColor];
+            remapArr[i] = remapColor;
+        }
+
+        var paletteArr = new Color32[256 * 256];
+        for (int i = 0; i < paletteArr.Length; i++)
+        {
+            paletteArr[i] = new Color32(255, 255, 255, 255);
+        }
+
+        var remapTex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        remapTex.filterMode = FilterMode.Point;
+        remapTex.SetPixels32(remapArr);
+        remapTex.Apply(false);
+        material.SetTexture("_RemapTex", remapTex);
+
+        paletteTex = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        paletteTex.filterMode = FilterMode.Point;
+        paletteTex.SetPixels32(paletteArr);
+        paletteTex.Apply(false);
+        material.SetTexture("_PaletteTex", paletteTex);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        var ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            var p = hitInfo.point;
+            int x = (int)Mathf.Floor(p.x) + width / 2;
+            int y = (int)Mathf.Floor(p.y) + height / 2;
+
+            var remapColor = remapArr[x + y * width];
+
+            if (!selectAny || !prevColor.Equals(remapColor))
+            {
+                if (selectAny)
+                {
+                    changeColor(prevColor, new Color32(255, 255, 255, 255));
+                }
+                selectAny = true;
+                prevColor = remapColor;
+                changeColor(remapColor, new Color32(50, 0, 255, 255));
+                paletteTex.Apply(false);
+            }
+        }
+    }
+
+    void changeColor(Color32 remapColor, Color32 showColor)
+    {
+        int xp = remapColor[0];
+        int yp = remapColor[1];
+
+        paletteTex.SetPixel(xp, yp, showColor);
+    }
+}
---
+++ Triumph/Assets/Test/MapShower.shader
@@ -0,0 +1,77 @@
+Shader "Unlit/MapShower"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _RemapTex ("Texture", 2D) = "white" {}
+        _PaletteTex ("Texture", 2D) = "white" {}
+        _TerrainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            sampler2D _RemapTex;
+            sampler2D _PaletteTex;
+            sampler2D _TerrainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                // return tex2D(_MainTex, i.uv) - tex2D(_MainTex, i.uv + float2(0.001, 0));
+                fixed4 c1 = tex2D(_MainTex, i.uv + float2(0.001, 0));
+                fixed4 c2 = tex2D(_MainTex, i.uv - float2(0.001, 0));
+                fixed4 c3 = tex2D(_MainTex, i.uv + float2(0, 0.001));
+                fixed4 c4 = tex2D(_MainTex, i.uv - float2(0, 0.001));
+
+                if(any(c1 != col) || any(c2 != col) || any(c3 != col) || any(c4 != col)){
+                    return fixed4(0,0,0,1);
+                }
+                fixed4 index = tex2D(_RemapTex, i.uv);
+                fixed4 paletteValue = tex2D(_PaletteTex, index.xy * 255.0 / 256.0 + float2(0.001953125, 0.001953125));
+                return pow(tex2D(_TerrainTex, i.uv), paletteValue.r);
+                // return fixed4(1,1,1,1);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
