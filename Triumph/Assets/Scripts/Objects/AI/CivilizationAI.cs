79370cd3264681866319fe9c1e7d23ee92d7f243
ProgrammingIsPunishment <134812626+ProgrammingIsPunishment@users.noreply.github.com>
2024-06-22 14:55:03 -0500

    Began adding debug menu for in game

--- Triumph/Assets/Scripts/Objects/AI/CivilizationAI.cs
+++ Triumph/Assets/Scripts/Objects/AI/CivilizationAI.cs
@@ -44,12 +44,31 @@ public class CivilizationAI
 
     public void CalculateInterests()
     {
-        //List<HoldingCluster> holdingClustersWithPresence = Oberkommando.VALUELIBRARY.HoldingValueSets.
+        List<Holding> ownedHoldings = new List<Holding>();
+        foreach (Holding h in Oberkommando.SAVE.Holdings) { if (h.Owner != null) { if (h.Owner.GUID == this.CoupledCivilization.GUID) { ownedHoldings.Add(h); } } }
+
+        List<HoldingCluster> holdingClustersWithPresence = Oberkommando.VALUELIBRARY.HoldingValueSets.Where(hvs => ownedHoldings.Any(oh => oh.GUID == hvs.HoldingGUD) == true).Select(hvs=>hvs.HoldingCluster).Distinct().ToList();
+
+        List<Holding> unownedHoldingsInClustersWithPresence = new List<Holding>();
+        foreach (HoldingValueSet hvs in Oberkommando.VALUELIBRARY.HoldingValueSets)
+        {
+            //Is holding unowned by civilization
+            if (holdingClustersWithPresence.Any(hcwp=>hcwp.Id != hvs.HoldingCluster.Id))
+            {
+                Holding holding = Oberkommando.SAVE.Holdings.Find(h=>h.GUID == hvs.HoldingGUD);
+                if (holding.TerrainType != TerrainType.Ocean)
+                {
+                    unownedHoldingsInClustersWithPresence.Add(holding);
+                }
+            }
+        }
+
         foreach (Interest i in this.Interests)
         {
             if (i.Holding != null)
             {
-                
+                if (unownedHoldingsInClustersWithPresence.Contains(i.Holding)) { i.TerritorialExpansionWeight++; }
+                i.TerritorialExpansionWeight++;
             }
         }
     }
